32933bfb530546aab9af43d1b0c2062be5ad0457
suren <tadevosyansuren07@gmail.com>
2024-08-16 01:57:03 +0400

    aws

--- task06/app/lambdas/audit_producer/index.js
+++ task06/app/lambdas/audit_producer/index.js
@@ -1,20 +1,25 @@
 const AWS = require("aws-sdk");
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const { v4: uuidv4 } = require("uuid");
 
 exports.handler = async (event) => {
   for (const record of event.Records) {
     if (record.eventName === "MODIFY" || record.eventName === "INSERT") {
-      const newItem = record.dynamodb.NewImage;
-      const oldItem = record.dynamodb.OldImage;
+      const newItem = AWS.DynamoDB.Converter.unmarshall(
+        record.dynamodb.NewImage
+      );
+      const oldItem = record.dynamodb.OldImage
+        ? AWS.DynamoDB.Converter.unmarshall(record.dynamodb.OldImage)
+        : null;
 
       const auditEntry = {
         TableName: "Audit",
         Item: {
-          id: newItem.id.S,
-          changeType: record.eventName,
+          id: uuidv4(),
+          itemKey: newItem.key || "UnknownKey",
+          modificationTime: new Date().toISOString(),
           newValue: newItem,
           oldValue: oldItem,
-          timestamp: new Date().toISOString(),
         },
       };
 
