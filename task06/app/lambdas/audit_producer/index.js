11c989460da4269a431873cb3deaca8c0cf3c3d0
suren <tadevosyansuren07@gmail.com>
2024-08-18 15:02:31 +0400

    aws

--- task06/app/lambdas/audit_producer/index.js
+++ task06/app/lambdas/audit_producer/index.js
@@ -1,29 +1,46 @@
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
 import AWS from "aws-sdk";
 import { v4 as uuidv4 } from "uuid";
 
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 export const handler = async (event) => {
-  for (const record of event.Records) {
-    if (record.eventName === "MODIFY" || record.eventName === "INSERT") {
-      const newItem = AWS.DynamoDB.Converter.unmarshall(
-        record.dynamodb.NewImage
-      );
-      const oldItem = record.dynamodb.OldImage
-        ? AWS.DynamoDB.Converter.unmarshall(record.dynamodb.OldImage)
-        : null;
+  try {
+    console.log("Event received:", JSON.stringify(event, null, 2));
 
-      const auditEntry = {
-        TableName: "Audit",
-        Item: {
-          id: uuidv4(),
-          itemKey: newItem.key || "UnknownKey",
-          modificationTime: new Date().toISOString(),
-          newValue: newItem,
-          oldValue: oldItem,
-        },
-      };
-
-      await dynamoDb.put(auditEntry).promise();
+    if (!event.Records || !Array.isArray(event.Records)) {
+      console.error("event.Records is not an array or is undefined");
+      return;
     }
+
+    for (const record of event.Records) {
+      console.log("Processing record:", JSON.stringify(record, null, 2));
+
+      if (record.eventName === "MODIFY" || record.eventName === "INSERT") {
+        const newItem = AWS.DynamoDB.Converter.unmarshall(
+          record.dynamodb.NewImage
+        );
+        const oldItem = record.dynamodb.OldImage
+          ? AWS.DynamoDB.Converter.unmarshall(record.dynamodb.OldImage)
+          : null;
+
+        const auditEntry = {
+          TableName: "Audit",
+          Item: {
+            id: uuidv4(),
+            itemKey: newItem.key || "UnknownKey",
+            modificationTime: new Date().toISOString(),
+            newValue: newItem,
+            oldValue: oldItem,
+          },
+        };
+
+        console.log("Audit entry:", JSON.stringify(auditEntry, null, 2));
+
+        await dynamoDb.put(auditEntry).promise();
+        console.log("Audit entry inserted successfully");
+      }
+    }
+  } catch (error) {
+    console.error("Error processing event:", error);
   }
 };
